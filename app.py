8b719abe07616a6e2d3f14aa50ca368e9e871e79
ilya <iokiselev31@gmail.com>
2022-11-29 22:49:43 +0300

    v2.0 done everything in task

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, session, flash, make_response
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timedelta
+from sqlalchemy import text
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///advertisements.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'f5a2b4f9c5cb15099b91a4252cbe8865fbb67df5'
 db = SQLAlchemy(app)
 
 
@@ -13,7 +15,7 @@ class Advertisement(db.Model):
     name = db.Column(db.String(300), nullable=False)
     description = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow())
-
+    session_number = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return '<Advertisement %r>' % self.id
@@ -22,7 +24,13 @@ class Advertisement(db.Model):
 @app.route('/')
 @app.route('/home')
 def index():
+    if 'visits' in session:
+        session['visits'] = session.get('visits') + 1
+    else:
+        session['visits'] = 1
     return render_template("index.html")
+
+
 
 
 @app.route('/about')
@@ -32,7 +40,17 @@ def about():
 
 @app.route('/posts')
 def posts():
-    adv = Advertisement.query.order_by(Advertisement.date.desc()).all()
+    with db.engine.connect() as connection:
+        result = connection.execute(text("SELECT * FROM advertisement"))
+        for row in result:
+            date_db = datetime.strptime(row["date"], '%Y-%m-%d %H:%M:%S.%f')
+            date_system = datetime.utcnow() - timedelta(minutes=5)
+            if date_system > date_db:
+                row_id = row["id"]
+                Advertisement.query.filter_by(id=row_id).delete()
+    db.session.commit()
+
+    adv = Advertisement.query.filter_by(session_number = session['visits'])
     return render_template("posts.html", adv=adv)
 
 
@@ -52,7 +70,6 @@ def post_delete(id):
         return redirect('/posts')
     except:
         "При удалении статьи произошла ошибка"
-
 
 
 @app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
@@ -76,8 +93,8 @@ def create_advertisement():
     if request.method == "POST":
         name = request.form['name']
         description = request.form['description']
-
-        adv = Advertisement(name=name, description=description)
+        session_number = session['visits']
+        adv = Advertisement(name=name, description=description, session_number=session_number)
 
         try:
             db.session.add(adv)
